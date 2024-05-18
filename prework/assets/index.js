2909418940f5a19e6ce6e98182d0a50a1f2cb39f
katelynabellera <148590382+katelynabellera@users.noreply.github.com>
2024-05-18 15:04:08 -0700

    Add files via upload

---
+++ prework/assets/index.js
@@ -0,0 +1,8 @@
+
+console.log("hello?")
+console.log("hello world!")
+
+let button = document.getElementById("btn")
+button.addEventListener("click", function() {
+    console.log("button is clicked")
+})
